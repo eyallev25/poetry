5f992051ec6285f66b01434f9319bc52b481d266
eyallev <eyallev@eyallev-Mac-1.local>
2021-12-25 08:43:21 +0200

    bla

--- test.sh
+++ test.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-(/root/.poetry/bin/poetry run pytest -vvv --suppress-tests-failed-exit-code --junitxml report.xml --html report.html) 2>&1 | tee output.log
+(/root/.poetry/bin/poetry run pytest -v -s -m dev -vvv --suppress-tests-failed-exit-code --junitxml report.xml --html report.html) 2>&1 | tee output.log
 /root/.poetry/bin/poetry run python3 -m demo_python
